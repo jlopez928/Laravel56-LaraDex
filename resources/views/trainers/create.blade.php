fabd527b3a5a8eaa031e7c612d6e639492fc6ce9
jesus lopez <jesus.lopez@venalum.com.ve>
2019-01-31 10:53:54 -0400

    12 - Curso de Laravel 5.6, Blade

--- resources/views/trainers/create.blade.php
+++ resources/views/trainers/create.blade.php
@@ -1,28 +1,16 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+@extends('layouts.app')
 
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
-</head>
-<body>
+@section('title', 'Trainers Create')
 
-    <div class="container">
-            <div class="form-group">
-                    <label for="">Nombre</label>
-                    <input type="text" class="form-control">
-            </div>
+@section('content')
 
-            <button type="submit" class="btn btn-primary">Guardar</button>
+<div class="container">
+    <div class="form-group">
+            <label for="">Nombre</label>
+            <input type="text" class="form-control">
     </div>
 
+    <button type="submit" class="btn btn-primary">Guardar</button>
+</div>
     
-    
-    <script src="{{ asset('js/jquery-3.3.1.slim.min.js') }}"></script>
-    <script src="{{ asset('js/popper.min.js') }}"></script>
-    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
-</body>
-</html>
+@endsection
