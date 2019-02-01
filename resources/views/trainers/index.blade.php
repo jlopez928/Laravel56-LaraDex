8001c415aaeff8755faa550e3b32fa5b33579d52
jesus lopez <jesus.lopez@venalum.com.ve>
2019-02-01 14:47:07 -0400

    19 - Curso de Laravel 5.6, Vista Trainers

--- resources/views/trainers/index.blade.php
+++ resources/views/trainers/index.blade.php
@@ -10,8 +10,8 @@
     <div class="row">
         @foreach ($trainers as $trainer)
             <div class="col-sm">
-                <div class="card" style="width: 18rem;">
-                    <img class="card-img-top" src="images/{{ $trainer->avatar }}" alt="">
+                <div class="card text-center" style="width: 18rem; margin-top:50px">
+                    <img style="height:100px; width:100px; background-color: #EFEFEF; margin:20px;" class="card-img-top rounded-circle mx-auto d-block" src="images/{{ $trainer->avatar }}" alt="">
                     <div class="card-body">
                         <h5 class="card-title">{{ $trainer->name }}</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card content.</p>
