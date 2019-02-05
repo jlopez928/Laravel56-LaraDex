5c0953e1a6a0dea646cb920b364d469e56868f5f
jesus lopez <jesus.lopez@venalum.com.ve>
2019-02-05 12:15:30 -0400

    35 - Curso de Laravel 5.6, Roles de usuario - Seeders

--- app/User.php
+++ app/User.php
@@ -11,7 +11,7 @@ class User extends Authenticatable
 
     public function roles() {
 
-        return $this->belongsToMany('App\Role');
+        return $this->belongsToMany('LaraDex\Role');
         
     }
 
