cf738baa70c6ebb3a78a5e07a02518ddf3209932
jesus lopez <jesus.lopez@venalum.com.ve>
2019-02-06 11:00:51 -0400

    43 - Curso de Laravel 5.6, Store laravel

--- app/Http/Controllers/PokemonController.php
+++ app/Http/Controllers/PokemonController.php
@@ -15,11 +15,27 @@ class PokemonController extends Controller
                 ['id' => 1, 'name' => 'Pikachu'],
                 ['id' => 2, 'name' => 'Pikachu2'],
                 ['id' => 3, 'name' => 'Pikachu3'],
-            ]);
+            ], 200);
         }
 
         return view('pokemons.index');
         
+    }
+
+    public function store(Request $request) {
+
+        if($request->ajax()) {
+
+            $pokemon = new Pokemon();
+            $pokemon->name = $request->input('name');
+            $pokemon->picture = $request->input('picture');
+            $pokemon->save();
+
+            return response()->json([
+                "message" => "Pokemon creado correctamente"
+            ], 200);
+
+        }
     }
 
 }
